ba0505bd873f34f5ab2b0aa77e6b7f60198d0c90
Abhinav <abhinavnirmalyam@gmail.com>
2024-07-16 11:27:10 +0530

    updated google Firebase function

--- src/component/OAuth.jsx
+++ src/component/OAuth.jsx
@@ -19,7 +19,8 @@ function OAuth() {
             headers:{
               "Content-Type" : "application/json"
             },
-            body:JSON.stringify({name :result.user.displayName,email:result.user.email,photo:result.user.photoURL})
+            body:JSON.stringify({name :result.user.displayName,email:result.user.email,photo:result.user.photoURL}),
+            credentials: 'include'
           })
           const data = await res.json()
           dispatch(signInSuccess(data))
